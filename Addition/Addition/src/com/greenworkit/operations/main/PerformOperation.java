58b99ddbcc6285ad175382a1d3e3e65b980045d1
Naveena <naveenajava12@gmail.com>
2020-03-24 13:20:11 -0400

    Java1-Intial Commit
    Commiting the changes as a part of java 1 ticket

---
+++ Addition/Addition/src/com/greenworkit/operations/main/PerformOperation.java
@@ -0,0 +1,13 @@
+package com.greenworkit.operations.main;
+
+public class PerformOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;
+		int b=20;
+		System.out.println("addition-----> :"+ (a+b));
+		
+	}
+
+}
